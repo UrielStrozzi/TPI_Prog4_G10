adc3acfbb6a8a98987b023b01beb6f1ba6da432f
Santino Ciccarelli <santinociccarelli19@gmail.com>
2026-06-16 18:58:26 -0300

    Segundo Commit

---
+++ src/test/java/prog4/g10/G10ApplicationTests.java
@@ -0,0 +1,13 @@
+package prog4.g10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
